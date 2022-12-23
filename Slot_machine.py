7a400671dd93497c6a15db44191c5180929616fa
Na-pewno-nie-Szymon <119611945+Na-pewno-nie-Szymon@users.noreply.github.com>
2022-12-23 13:40:06 +0100

    Updated

--- Slot_machine.py
+++ Slot_machine.py
@@ -1,15 +1,47 @@
+""" |---| READ ME |---|
+
+Cześć jest to projekt, w którym staram się napisać maszynę losującą niczym z kasyn.
+
+Program ma przechowywać dane pomiędzy uruchomieniami, tzn że jak raz się zarejestrujemy to
+będziemy mogli już do końca naszych dni (albo do końca dobrej woli admina) kożystać z tego konta
+
+Do maszyny wprowadzamy nasze dane logowania, wpłacamy wirtualne pieniądze i możemy grać!
+
+Projekt jest cały czas w trakcie rozwijania także jakbyś natrafił/a na jakiś bug/error to można się
+ze mną kontaktować przez Discord: Access#2933
+
+Instrukcja obsługi:
+    - Na początku trzeba ręcznie zrobić sobie 3 pliki tekstowe:
+        - database.txt - tam będziemy przechowywać dane użytkowników RAZEM Z ICH HASŁAMI!!!
+        - earnings.txt - tutaj przechowujemy informacje ile nasza maszyna zarobiła pieniędzy
+        - logs.txt - JESZCZE nie jest potrzebny ale niebawem będziemy mogli znaleźć tam logi użytkowników
+    - należy też pozmieniać nazwy ścieżek tak aby pasowały wyżej stworzonym folderom
+    - do przemieszczania się po tym prostym GUI używamy odpowiednich cyfr
+    - aby dostać się do panelu administratora należy wylogować się ze wszystkich kont
+      a następnie wpisać "Admin1"
+"""
+
+
 """TODO
-* Implement simple txt database                               | in progress   |
-* Implement logs only last 200 interactions                   | in progress   |
-* Implement deposit value database                            | in progress   |
-* Make BIG RED BUTTON that clears every database              | in progress   |
-* User stats - games played - money gain - admin access only  | in progress   |
+|     DONE!     |   Implement simple txt database
+|     DONE!     |   Implement total earning counter
+|  in progress  |   Implement logs only last 200 interactions
+|  in progress  |   Implement deposit value database
+|  in progress  |   Make BIG RED BUTTON that clears every database
+|  in progress  |   User stats - games played - money gain - admin access only
 """
 
 import random
+
 print('\n\t\tWORK STILL IN PROGRESS\n\n\n')
-user_dataBase = [['Admin1', 'Admin1', 10000000.0]]
-total_deposits = 0.0
+
+
+def database_assignment():
+    with open("venv/slm_data/database.txt", "r") as database:
+        user_dataBase = database.readlines()
+    for idx, val in enumerate(user_dataBase):
+        user_dataBase[idx] = val.split()
+    return user_dataBase
 
 
 def sing_in(database):
@@ -28,19 +60,27 @@ def sing_in(database):
             break
         else:
             print('Wrong amount, please try again.')
-    user = [username, passwd, money]
+    user = f'{username} {passwd} {money}'
     return user
 
 
 def log_in(username, passwd, user):
-    if username == user[0] and passwd == user[1]: return 1
+    if username == user[0] and passwd == user[1]:
+        return 1
     return 0
 
 
 def deposit():
     while True:
+        current_money = 0.0
+        with open('venv/slm_data/earnings.txt', 'r') as money_file:
+            current_money = float(money_file.read())
         depo = float(input('Deposit at least 5$: '))
+        print(current_money)
         if depo >= 5:
+            current_money += depo
+            with open('venv/slm_data/earnings.txt', 'w') as file:
+                file.write(f'{current_money}')
             return depo
         else:
             print('Wrong amount! \nIf You do not want to deposit press (q)')
@@ -93,37 +133,40 @@ def slot_machine(money):
 
 
 def simple_logged_interface(username, money_amount):
-    money_amount = money_amount
     while True:
-        case = int(input(f'Welcome {username}, you have {money_amount}$!\nLet{chr(92)}s play!\n\n1. Deposit\n2. Play\n3. '
-                         f'Quit\n'))
+        case = int(input(f'Welcome {username}, you have {money_amount}$!\nLet{chr(92)}s play!\n\n1. Deposit\n2. '
+                         f'Play\n3. Quit\n'))
         if case == 1:
-            depo = deposit()
-            money_amount += depo
-           # total_deposits += depo
+            money_amount += deposit()
         elif case == 2:
             money_amount = slot_machine(money_amount)
         elif case == 3:
             return money_amount
 
 
-def p_user_database():
+def p_user_database(DB):
     print('username - passwd - wallet')
-    for user in user_dataBase:
+    for user in DB:
         print(*user, sep='\t | \t')
     return 0
 
 
-def admin_panel():
+def admin_panel(DB):
     while True:
-        case = input('1. User Database\n2. Earnings\n3. Logs\n4. RED_BUTTON_(DO NOT USE!!)')
+        case = input('1. User Database\n2. Earnings\n3. Logs\n4. Exit admin panel\n213769420. RED_BUTTON_(DO NOT USE!!)\n')
         if case == '1':
-            p_user_database()
+            p_user_database(DB)
+        elif case == '2':
+            with open('venv/slm_data/earnings.txt', 'r') as money:
+                cash = money.read()
+                print(f'users deposited {cash}$')
+        elif case == '4':
             break
 
 
 def simple_start_interface():
     while True:
+        user_dataBase = database_assignment()
         case = input('1. Login\n2. Sign in\n3. Close program\n')
         if case == '1':
             flag = False
@@ -131,7 +174,7 @@ def simple_start_interface():
             passwd = input('Password:\t')
             for i in user_dataBase:
                 if log_in(username, passwd, i):
-                    i[2] = simple_logged_interface(username, i[2])
+                    i[2] = simple_logged_interface(username, float(i[2]))
                     flag = True
                     break
             if not flag:
@@ -139,9 +182,11 @@ def simple_start_interface():
         elif case == '3':
             exit()
         elif case == '2':
-            user_dataBase.append(sing_in(user_dataBase))
+            with open('venv/slm_data/database.txt', 'a') as database:
+                database.write(sing_in(user_dataBase)+'\n')
+
         elif case == 'Admin1':
-            admin_panel()
+            admin_panel(user_dataBase)
         else:
             print('Error!!!')
 
